cf3e0f437bb66aaa54b252385c92116eb345bab6
evgeniybryzh <61083851+evgeniybryzh@users.noreply.github.com>
2020-06-17 17:32:28 +0300

    Add autocomplete

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,9 @@ import { changeBG } from "./scripts/bg";
 import { getResponse } from "./scripts/api";
 import { useMap } from "./scripts/map";
 import { changeBGForMobile } from "./scripts/mobile-bg";
+import { useAutocomplete } from "./scripts/autocomplete";
 changeBGForMobile();
 useBurgerMenu();
 changeBG();
 getResponse();
-
+useAutocomplete();
---
+++ src/js/scripts/autocomplete.js
@@ -0,0 +1,18 @@
+export const useAutocomplete = () => {
+  const placesOptions = {
+    appId: "pl58FNVF6UP2",
+    apiKey: "e431d60fe4e979e236ef9cd6fd0c9e3d",
+    container: document.getElementById("input"),
+    templates: {
+      value: function (suggestion) {
+        return suggestion.name;
+      },
+    },
+  };
+  const reconfigurableOptions = {
+    type: "city",
+    aroundLatLngViaIP: false,
+  };
+
+  const placesInstance = places(placesOptions).configure(reconfigurableOptions);
+};
