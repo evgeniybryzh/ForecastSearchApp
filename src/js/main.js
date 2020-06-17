6b378f96e4e67a94b06da498b2fc8736d1b70125
evgeniybryzh <61083851+evgeniybryzh@users.noreply.github.com>
2020-06-18 00:34:46 +0300

    add animations

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,16 @@
 "use strict";
+
 import { useBurgerMenu } from "./scripts/burger";
 import { changeBG } from "./scripts/bg";
 import { getResponse } from "./scripts/api";
 import { useMap } from "./scripts/map";
 import { changeBGForMobile } from "./scripts/mobile-bg";
 import { useAutocomplete } from "./scripts/autocomplete";
+import { usePreloader } from "./scripts/preloader";
+
 changeBGForMobile();
 useBurgerMenu();
 changeBG();
 getResponse();
 useAutocomplete();
+usePreloader();
